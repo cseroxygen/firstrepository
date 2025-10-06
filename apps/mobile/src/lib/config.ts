9d7874a3ae9472e30785b1cb441ed91a5e8f3925
Sai Wang <saiwang@SaideMacBook-Air.local>
2025-10-06 22:57:16 +0200

    Switch mobile API base to https://api.homeref.online

--- apps/mobile/src/lib/config.ts
+++ apps/mobile/src/lib/config.ts
@@ -3,5 +3,5 @@ import Constants from 'expo-constants'
 const extras = (Constants?.expoConfig as any)?.extra || {}
 
 export function defaultApiBase() {
-  return extras.apiBase || 'http://api.homeref.online:8000'
+  return extras.apiBase || 'https://api.homeref.online'
 }
