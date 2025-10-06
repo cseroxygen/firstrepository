5c9a5d99439e98479b8a4caa283870f15ef2de15
Sai Wang <saiwang@SaideMacBook-Air.local>
2025-10-06 22:00:36 +0200

    Point mobile API base to api.homeref.online

--- apps/mobile/src/lib/config.ts
+++ apps/mobile/src/lib/config.ts
@@ -3,5 +3,5 @@ import Constants from 'expo-constants'
 const extras = (Constants?.expoConfig as any)?.extra || {}
 
 export function defaultApiBase() {
-  return extras.apiBase || 'http://46.101.240.53:8000'
+  return extras.apiBase || 'http://api.homeref.online:8000'
 }
